bcef16c8e2dd6982c49766047ea8146cde34979c
Duy Phung <>
2024-11-15 12:46:43 +0800

    change color method

--- Sources/FaceLiveness/Views/Liveness/_FaceLivenessDetectionView.swift
+++ Sources/FaceLiveness/Views/Liveness/_FaceLivenessDetectionView.swift
@@ -54,9 +54,9 @@ struct _FaceLivenessDetectionView<VideoView: View>: View {
                 .padding([.leading, .trailing])
                 .aspectRatio(3/4, contentMode: .fit)
                 .frame(maxWidth: .infinity)
-                .background(colors.livenessBackground)
             }
         }
+        .background(colors.livenessBackground)
         .edgesIgnoringSafeArea(.all)
     }
 }
